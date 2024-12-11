7d9a67d990d7ab524a2552a426a0481a33231f8c
priti <pritichaugule44@gmail.com>
2024-12-11 21:47:57 +0530

    python basic code

---
+++ list.py
@@ -0,0 +1,36 @@
+# Que-1
+
+list=[1,2,3,4,5,2,1,5,8]
+
+# 1.len 
+# print(len(list))
+
+# 2.max
+# print(max(list))
+
+# 3.append
+# list.append(10)
+# print(list)
+
+# 4.sort
+# list.sort()
+# print(list)
+
+# 5.reverse
+# list.reverse()
+# print(list)
+
+# 6.index
+# A=list.index(5)
+# print(A)
+
+# 7.insert
+# list.insert(4,12)
+# print(list)
+
+# 8.pop
+# print(list.pop(5))
+
+# 9.count
+count=list.count(5)
+print(count)
---
+++ set&tuple.py
@@ -0,0 +1,27 @@
+# tuple
+
+# 1.index
+# tuple=(1,2,3,4,5,6)
+# print(tuple.index(5))
+
+# 2.count
+# print(tuple.count(3))
+
+### set ###
+
+set={1,2,3,4,5,6,8,9}
+# print(dir(set))
+
+# 1.add
+# set.add(20)
+# print(set) 
+
+
+# # 2.remove 
+# set.remove(8)
+# print(set)
+
+# 3.union
+set1={20,30,20,40}
+union=set.union(set1)
+print(union)
