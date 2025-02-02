ca23a3de91abd2fd6efeef7a2c36e1b1df977cc3
priti <pritichaugule44@gmail.com>
2025-02-02 16:03:03 +0530

    list and set

---
+++ list.py
@@ -0,0 +1,7 @@
+a=[1,2,3,4]
+b=["hello","world","hii","priti"]
+c=[]
+
+for i in range(len(a)):
+    c.append((a[i],b[i]))
+print(c)
---
+++ set.py
@@ -0,0 +1,10 @@
+#add 
+set={1,2,3,4,5,6}
+# set.add(12)
+# print(set)
+
+
+#remove
+set.remove(5)
+print(set)
+
